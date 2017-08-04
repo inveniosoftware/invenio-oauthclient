e9d850efae2de747c401d513bab9e5154e71618b
Diego Rodriguez <diego.rodriguez@cern.ch>
2017-08-04 14:13:24 +0200

    global: update to last cookiecutter
    Signed-off-by: Diego Rodriguez <diego.rodriguez@cern.ch>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015, 2016 CERN.
+# Copyright (C) 2015, 2016, 2017 CERN.
 #
 # Invenio is free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public License as
@@ -18,9 +18,9 @@
 # Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 # MA 02111-1307, USA.
 #
-# In applying this license, CERN does not waive the privileges and immunities
-# granted to it by virtue of its status as an Intergovernmental Organization
-# or submit itself to any jurisdiction.
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
 
 """Invenio module that provides OAuth web authorization support."""
 
@@ -101,6 +101,7 @@ install_requires = [
 ]
 
 packages = find_packages()
+
 
 # Get the version string. Cannot be done with import!
 g = {}
