91c37b9d48cf83944536044c3e2e1d2c26b60a8b
Pablo Panero <pablo.panero@cern.ch>
2020-06-03 13:33:59 +0200

    handlers: export rest handlers

--- invenio_oauthclient/handlers/__init__.py
+++ invenio_oauthclient/handlers/__init__.py
@@ -10,6 +10,11 @@
 
 from __future__ import absolute_import, print_function
 
+from .rest import authorized_default_handler as \
+    authorized_default_handler_rest, \
+    authorized_signup_handler as authorized_signup_handler_rest, \
+    disconnect_handler as disconnect_handler_rest, \
+    signup_handler as signup_handler_rest
 from .ui import authorized_default_handler, authorized_signup_handler, \
     disconnect_handler, oauth2_handle_error, \
     oauth_resp_remote_error_handler as oauth_error_handler, signup_handler
@@ -19,9 +24,12 @@ from .utils import authorized_handler, get_session_next_url, make_handler, \
     token_delete, token_getter, token_session_key, token_setter
 
 __all__ = (
+    'authorized_default_handler_rest',
     'authorized_default_handler',
     'authorized_handler',
+    'authorized_signup_handler_rest',
     'authorized_signup_handler',
+    'disconnect_handler_rest',
     'disconnect_handler',
     'get_session_next_url',
     'make_handler',
@@ -33,6 +41,7 @@ __all__ = (
     'oauth2_token_setter',
     'response_token_setter',
     'set_session_next_url',
+    'signup_handler_rest',
     'signup_handler',
     'token_delete',
     'token_getter',
