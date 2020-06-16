a0b679de5e6da4e9053a84ae17a30f16572e34b4
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2020-06-16 10:43:29 +0200

    contrib: fix orcid configuration
    * Adds error message when a remote server error occurs.
    
    Signed-off-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>

--- invenio_oauthclient/contrib/orcid.py
+++ invenio_oauthclient/contrib/orcid.py
@@ -91,7 +91,7 @@ BASE_APP = dict(
                               'show_login': 'true'},
         base_url='https://pub.orcid.org/v1.2/',
         request_token_url=None,
-        access_token_url="https://api.orcid.org/oauth/token",
+        access_token_url="https://pub.orcid.org/oauth/token",
         access_token_method='POST',
         authorize_url='https://orcid.org/oauth/authorize',
         app_key='ORCID_APP_CREDENTIALS',
