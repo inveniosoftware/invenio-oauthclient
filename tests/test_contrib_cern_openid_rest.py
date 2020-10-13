7ffd05ecf8da183416f25b6494f8a73b4215c37b
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2020-10-13 09:06:20 +0200

    contrib: handle login via token

--- tests/test_contrib_cern_openid_rest.py
+++ tests/test_contrib_cern_openid_rest.py
@@ -11,9 +11,11 @@
 from __future__ import absolute_import
 
 import os
+from datetime import timedelta
 
 import pytest
-from flask import g, session, url_for
+from flask import current_app, g, session, url_for
+from flask_login import current_user
 from flask_security import login_user, logout_user
 from helpers import check_response_redirect_url_args, get_state, \
     mock_remote_get, mock_response
@@ -22,6 +24,7 @@ from six.moves.urllib_parse import parse_qs, urlparse
 from invenio_oauthclient.contrib.cern_openid import \
     OAUTHCLIENT_CERN_OPENID_SESSION_KEY, account_info_rest, \
     disconnect_rest_handler, fetch_extra_data, get_dict_from_response
+from invenio_oauthclient.models import RemoteAccount
 
 from flask_oauthlib.client import OAuthResponse  # noqa isort:skip
 
@@ -196,3 +199,93 @@ def test_account_info_not_allowed_account(app_rest, example_cern_openid_rest):
         "code": 400,
     }
     check_response_redirect_url_args(resp, expected_url_args)
+
+
+def test_identity_changed(app_rest, example_cern_openid_rest, models_fixture):
+    def _init():
+        ioc = app_rest.extensions['oauthlib.client']
+
+        # setup the user account via cern_openid
+        with app_rest.test_client() as c:
+            # Ensure remote apps have been loaded (due to before first request)
+            resp = c.get(url_for('invenio_oauthclient.rest_login',
+                                 remote_app='cern_openid'))
+            assert resp.status_code == 302
+
+            example_response, example_token, example_account_info = \
+                example_cern_openid_rest
+
+            mock_response(app_rest.extensions['oauthlib.client'],
+                          'cern_openid',
+                          example_token)
+            mock_remote_get(ioc, 'cern_openid', example_response)
+
+            resp = c.get(url_for(
+                'invenio_oauthclient.rest_authorized',
+                remote_app='cern_openid', code='test',
+                state=get_state('cern_openid')))
+            assert resp.status_code == 302
+            expected_url_args = {
+                "message": "Successfully authorized.",
+                "code": 200,
+            }
+            check_response_redirect_url_args(resp, expected_url_args)
+
+            assert len(g.identity.provides) == 3
+
+    def _test_with_token(user, remote_account):
+        with app_rest.test_request_context():
+            # mark user as logged in via token
+            user.login_via_oauth2 = True
+            # check if the initial roles are there
+            login_user(user)
+
+            assert current_user.login_via_oauth2
+
+            assert len(g.identity.provides) == 3
+            logout_user()
+
+            # remove the cern roles
+            remote_account.extra_data.update(roles=[])
+
+            # login the user again
+            login_user(user)
+
+            # check if the cern roles are not fetched from the provider
+            assert len(g.identity.provides) == 2
+            logout_user()
+
+    def _test_without_token(user, remote_account):
+        user.login_via_oauth2 = False
+
+        current_app.config[
+            'OAUTHCLIENT_CERN_OPENID_REFRESH_TIMEDELTA'] = False
+        login_user(user)
+
+        # check that the roles are not refreshed from provider
+        assert len(g.identity.provides) == 2
+        logout_user()
+
+        current_app.config['OAUTHCLIENT_CERN_OPENID_REFRESH_TIMEDELTA'] \
+            = timedelta(microseconds=1)
+        login_user(user)
+
+        # check if roles refreshed from the provider
+        assert len(g.identity.provides) == 3
+
+    _init()
+
+    datastore = app_rest.extensions['invenio-accounts'].datastore
+    user = datastore.find_user(email='john.doe@cern.ch')
+    assert user
+
+    client_id = current_app.config["CERN_APP_OPENID_CREDENTIALS"][
+        "consumer_key"
+    ]
+    # make sure the roles are cleaned
+    remote_account = RemoteAccount.get(
+        user_id=user.get_id(), client_id=client_id
+    )
+
+    _test_with_token(user, remote_account)
+    _test_without_token(user, remote_account)
