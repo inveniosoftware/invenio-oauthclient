00e46784b6e7b6058d193a5b98746a2df5fc6ec3
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2020-12-09 23:54:39 +0100

    tests: fix run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -16,10 +16,15 @@ set -o errexit
 # Quit on unbound symbols
 set -o nounset
 
+# Always bring down docker services
+cleanup() {
+    eval "$(docker-services-cli down --env)"
+}
+trap cleanup EXIT
+
 python -m check_manifest --ignore ".*-requirements.txt"
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
-docker-services-cli up ${DB}
+eval "$(docker-services-cli up --db ${DB:-postgresql} --env)"
 python -m pytest
 tests_exit_code=$?
-docker-services-cli down
 exit "$tests_exit_code"
