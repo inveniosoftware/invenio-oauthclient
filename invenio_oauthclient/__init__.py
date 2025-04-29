1444aeb717fa60a96716a1b76b35444388653842
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-04-29 22:36:27 +0200

    release: v4.2.0

--- invenio_oauthclient/__init__.py
+++ invenio_oauthclient/__init__.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2023 CERN.
-# Copyright (C) 2024 Graz University of Technology.
+# Copyright (C) 2024-2025 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -13,7 +13,7 @@ from .ext import InvenioOAuthClient, InvenioOAuthClientREST
 from .oauth import oauth_link_external_id, oauth_unlink_external_id
 from .proxies import current_oauthclient
 
-__version__ = "4.1.4"
+__version__ = "4.2.0"
 
 __all__ = (
     "__version__",
