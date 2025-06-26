d46d3ac37e68a354c8cf35c68f8af34c41ab5538
Alex Ioannidis <a.ioannidis@cern.ch>
2025-06-26 15:10:35 +0200

    contrib: add EOSC EU Node AAI OIDC provider

---
+++ invenio_oauthclient/contrib/eosc_aai.py
@@ -0,0 +1,320 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2025 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Pre-configured remote application for enabling sign in/up with EOSC AAI.
+
+1. Edit your configuration and add:
+
+   .. code-block:: python
+
+       from invenio_oauthclient.contrib import eosc_aai
+
+       OAUTHCLIENT_REMOTE_APPS = dict(
+           eosc_aai=eosc_aai.REMOTE_APP,
+       )
+
+       EOSC_AAI_APP_CREDENTIALS = dict(
+           consumer_key="changeme",
+           consumer_secret="changeme",
+       )
+
+  Note, if you want to use other environments:
+  - For testing: ``eosc_aai.REMOTE_TESTING_APP``
+  - For staging: ``eosc_aai.REMOTE_STAGING_APP``
+  - For production: ``eosc_aai.REMOTE_APP`` (default)
+
+2. Register a new application with EOSC AAI. Visit the appropriate Service
+   Registration URL for your environment:
+
+   - Testing: https://webapp.testing.eosc-federation.eu/sp_request
+   - Staging: https://webapp.staging.eosc-federation.eu/sp_request
+   - Production: https://webapp.aai.open-science-cloud.ec.europa.eu/sp_request
+
+   When registering ensure that the *Redirect URI* points to:
+   ``CFG_SITE_URL/oauth/authorized/eosc_aai/`` (e.g.
+   ``https://localhost:5000/oauth/authorized/eosc_aai/``).
+
+3. Grab the *Client ID* and *Client Secret* after registering the application
+   and add them to your instance configuration (``invenio.cfg``):
+
+   .. code-block:: python
+
+       EOSC_AAI_APP_CREDENTIALS = dict(
+           consumer_key="<CLIENT ID>",
+           consumer_secret="<CLIENT SECRET>",
+       )
+
+4. Now go to ``CFG_SITE_URL/oauth/login/eosc_aai/`` (e.g.
+   http://localhost:4000/oauth/login/eosc_aai/)
+
+5. Also, you should see EOSC AAI listed under Linked accounts:
+   http://localhost:4000/account/settings/linkedaccounts/
+
+By default the EOSC AAI module will try first look if a link already exists
+between a EOSC AAI account and a user. If no link is found, the user is asked
+to provide an email address to sign-up.
+
+In templates you can add a sign in/up link:
+
+.. code-block:: jinja
+
+    <a href="{{url_for('invenio_oauthclient.login', remote_app='eosc_aai')}}">
+      Sign in with EOSC AAI
+    </a>
+
+"""
+
+from flask import current_app, redirect, url_for
+from flask_login import current_user
+from invenio_db import db
+from invenio_i18n import lazy_gettext as _
+
+from invenio_oauthclient import current_oauthclient
+from invenio_oauthclient.contrib.settings import OAuthSettingsHelper
+from invenio_oauthclient.handlers.rest import response_handler
+from invenio_oauthclient.handlers.utils import require_more_than_one_external_account
+from invenio_oauthclient.models import RemoteAccount
+from invenio_oauthclient.oauth import oauth_link_external_id, oauth_unlink_external_id
+
+
+class EOSCAAIOAuthSettingsHelper(OAuthSettingsHelper):
+    """Default configuration for EOSC AAI OAuth provider."""
+
+    def __init__(
+        self,
+        title=None,
+        description=None,
+        base_url=None,
+        app_key=None,
+        precedence_mask=None,
+        signup_options=None,
+    ):
+        """Constructor."""
+        precedence_mask = precedence_mask or {
+            "email": False,
+        }
+        signup_options = signup_options or {
+            "auto_confirm": False,
+            "send_register_msg": True,
+        }
+
+        super().__init__(
+            title or _("EOSC AAI"),
+            description
+            or _(
+                "European Open Science Cloud Authentication and Authorization Infrastructure."
+            ),
+            base_url=base_url,
+            app_key=app_key or "EOSC_AAI_APP_CREDENTIALS",
+            request_token_params={
+                "scope": "openid profile email",
+            },
+            access_token_url=f"{base_url}/OIDC/token",
+            authorize_url=f"{base_url}/OIDC/authorization",
+            content_type="application/json",
+            precedence_mask=precedence_mask,
+            signup_options=signup_options,
+        )
+
+        self._handlers = dict(
+            authorized_handler="invenio_oauthclient.handlers:authorized_signup_handler",
+            disconnect_handler="invenio_oauthclient.contrib.eosc_aai:disconnect_handler",
+            signup_handler=dict(
+                info="invenio_oauthclient.contrib.eosc_aai:account_info",
+                info_serializer="invenio_oauthclient.contrib.eosc_aai:account_info_serializer",
+                setup="invenio_oauthclient.contrib.eosc_aai:account_setup",
+                view="invenio_oauthclient.handlers:signup_handler",
+            ),
+        )
+
+        self._rest_handlers = dict(
+            authorized_handler="invenio_oauthclient.handlers.rest:authorized_signup_handler",
+            disconnect_handler="invenio_oauthclient.contrib.eosc_aai:disconnect_rest_handler",
+            signup_handler=dict(
+                info="invenio_oauthclient.contrib.eosc_aai:account_info",
+                info_serializer="invenio_oauthclient.contrib.eosc_aai:account_info_serializer",
+                setup="invenio_oauthclient.contrib.eosc_aai:account_setup",
+                view="invenio_oauthclient.handlers.rest:signup_handler",
+            ),
+            response_handler="invenio_oauthclient.handlers.rest:default_remote_response_handler",
+            authorized_redirect_url="/",
+            disconnect_redirect_url="/",
+            signup_redirect_url="/",
+            error_redirect_url="/",
+        )
+
+    def get_handlers(self):
+        """Return EOSC AAI auth handlers."""
+        return self._handlers
+
+    def get_rest_handlers(self):
+        """Return EOSC AAI auth REST handlers."""
+        return self._rest_handlers
+
+
+# Production environment (default)
+_eosc_aai_app = EOSCAAIOAuthSettingsHelper(
+    base_url="https://proxy.aai.open-science-cloud.ec.europa.eu",
+)
+
+BASE_APP = _eosc_aai_app.base_app
+REMOTE_APP = _eosc_aai_app.remote_app
+"""EOSC AAI Production Remote Application."""
+
+REMOTE_REST_APP = _eosc_aai_app.remote_rest_app
+"""EOSC AAI Production Remote REST Application."""
+
+
+# Testing environment
+_eosc_aai_testing_app = EOSCAAIOAuthSettingsHelper(
+    base_url="https://proxy.testing.eosc-federation.eu",
+)
+REMOTE_TESTING_APP = _eosc_aai_testing_app.remote_app
+"""EOSC AAI Testing Remote Application."""
+
+REMOTE_TESTING_REST_APP = _eosc_aai_testing_app.remote_rest_app
+"""EOSC AAI Testing Remote REST Application."""
+
+
+# Staging environment
+_eosc_aai_staging_app = EOSCAAIOAuthSettingsHelper(
+    base_url="https://proxy.staging.eosc-federation.eu",
+)
+REMOTE_STAGING_APP = _eosc_aai_staging_app.remote_app
+"""EOSC AAI Staging Remote Application."""
+
+REMOTE_STAGING_REST_APP = _eosc_aai_staging_app.remote_rest_app
+"""EOSC AAI Staging Remote REST Application."""
+
+
+def account_info_serializer(remote, resp, **kwargs):
+    """Serialize the account info response object.
+
+    :param remote: The remote application.
+    :param resp: The response of the `authorized` endpoint.
+    :returns: A dictionary with serialized user information.
+    """
+    return {
+        "external_id": resp.get("sub"),
+        "external_method": remote.name,
+        "user": {
+            "email": resp.get("email"),
+            "profile": {
+                "full_name": resp.get("name"),
+                "given_name": resp.get("given_name"),
+                "family_name": resp.get("family_name"),
+            },
+        },
+        "active": True,
+    }
+
+
+def account_info(remote, resp):
+    """Retrieve remote account information used to find local user.
+
+    It returns a dictionary with the following structure:
+
+    .. code-block:: python
+
+        {
+            'user': {
+                'email': 'user@example.org',
+                'profile': {
+                    'full_name': 'Full Name',
+                    'given_name': 'First',
+                    'family_name': 'Last',
+                },
+            },
+            'external_id': 'eosc-aai-user-identifier',
+            'external_method': 'eosc_aai',
+            'active': True,
+        }
+
+    :param remote: The remote application.
+    :param resp: The response of the `authorized` endpoint.
+    :returns: A dictionary with the user information.
+    """
+    handlers = current_oauthclient.signup_handlers[remote.name]
+    # `remote` param automatically injected via `make_handler` helper
+    return handlers["info_serializer"](resp)
+
+
+@require_more_than_one_external_account
+def _disconnect(remote, *args, **kwargs):
+    """Handle unlinking of remote account.
+
+    :param remote: The remote application.
+    """
+    if not current_user.is_authenticated:
+        return current_app.login_manager.unauthorized()
+
+    account = RemoteAccount.get(
+        user_id=current_user.get_id(), client_id=remote.consumer_key
+    )
+    eosc_sub = account.extra_data.get("sub") if account else None
+
+    if eosc_sub:
+        oauth_unlink_external_id({"id": eosc_sub, "method": "eosc_aai"})
+    if account:
+        with db.session.begin_nested():
+            account.delete()
+
+
+def disconnect_handler(remote, *args, **kwargs):
+    """Handle unlinking of remote account.
+
+    :param remote: The remote application.
+    """
+    _disconnect(remote, *args, **kwargs)
+    return redirect(url_for("invenio_oauthclient_settings.index"))
+
+
+def disconnect_rest_handler(remote, *args, **kwargs):
+    """Handle unlinking of remote account.
+
+    :param remote: The remote application.
+    """
+    _disconnect(remote, *args, **kwargs)
+    redirect_url = current_app.config["OAUTHCLIENT_REST_REMOTE_APPS"][remote.name][
+        "disconnect_redirect_url"
+    ]
+    return response_handler(remote, redirect_url)
+
+
+def account_setup(remote, token, resp):
+    """Perform additional setup after user have been logged in.
+
+    :param remote: The remote application.
+    :param token: The token value.
+    :param resp: The response.
+    """
+    with db.session.begin_nested():
+        # Retrieve EOSC AAI data from response
+        sub = resp.get("sub")
+        full_name = resp.get("name")
+        given_name = resp.get("given_name")
+        family_name = resp.get("family_name")
+        email = resp.get("email")
+        eunode_projects = resp.get("eunode_projects", [])
+        entitlements = resp.get("entitlements", [])
+
+        # Set EOSC AAI data in extra_data
+        token.remote_account.extra_data = {
+            "sub": sub,
+            "full_name": full_name,
+            "given_name": given_name,
+            "family_name": family_name,
+            "email": email,
+            "eunode_projects": eunode_projects,
+            "entitlements": entitlements,
+        }
+
+        user = token.remote_account.user
+
+        # Create user <-> external id link
+        oauth_link_external_id(user, {"id": sub, "method": "eosc_aai"})
---
+++ tests/test_contrib_eosc_aai.py
@@ -0,0 +1,382 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2025 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Test case for EOSC AAI oauth remote app."""
+
+from urllib.parse import parse_qs, urlparse
+
+from flask import session, url_for
+from flask_login import current_user
+from flask_security import login_user
+from flask_security.utils import hash_password
+from helpers import get_state, mock_response
+from invenio_accounts.models import User
+from invenio_db import db
+
+from invenio_oauthclient.contrib.eosc_aai import account_info
+from invenio_oauthclient.handlers import token_session_key
+from invenio_oauthclient.models import RemoteAccount, RemoteToken, UserIdentity
+
+
+def test_account_info(app, example_eosc_aai):
+    """Test account info extraction."""
+    client = app.test_client()
+    ioc = app.extensions["oauthlib.client"]
+    # Ensure remote apps have been loaded (due to before first request)
+    client.get(url_for("invenio_oauthclient.login", remote_app="eosc_aai"))
+
+    example_data, example_account_info = example_eosc_aai
+
+    assert (
+        account_info(ioc.remote_apps["eosc_aai"], example_data) == example_account_info
+    )
+
+    assert account_info(ioc.remote_apps["eosc_aai"], {}) == dict(
+        external_id=None,
+        external_method="eosc_aai",
+        user=dict(
+            email=None,
+            profile=dict(
+                full_name=None,
+                given_name=None,
+                family_name=None,
+            ),
+        ),
+        active=True,
+    )
+
+
+def test_login(app):
+    """Test EOSC AAI login."""
+    client = app.test_client()
+
+    resp = client.get(
+        url_for("invenio_oauthclient.login", remote_app="eosc_aai", next="/someurl/")
+    )
+    assert resp.status_code == 302
+
+    params = parse_qs(urlparse(resp.location).query)
+    assert params["response_type"] == ["code"]
+    assert params["scope"] == ["openid profile email"]
+    assert params["redirect_uri"]
+    assert params["client_id"]
+    assert params["state"]
+
+
+def test_authorized_signup(app_with_userprofiles, example_eosc_aai):
+    """Test authorized callback with sign-up."""
+    app = app_with_userprofiles
+    example_data, _ = example_eosc_aai
+
+    # Create OAuth token response format with user info
+    oauth_token_response = {
+        "access_token": "test_access_token",
+        "token_type": "Bearer",
+        "expires_in": 3600,
+        "refresh_token": "test_refresh_token",
+        "scope": "openid profile email",
+        # OpenID Connect includes user claims in token response
+        **example_data,
+    }
+
+    with app.test_client() as c:
+        # Ensure remote apps have been loaded
+        resp = c.get(url_for("invenio_oauthclient.login", remote_app="eosc_aai"))
+        assert resp.status_code == 302
+
+        # Mock the response from EOSC AAI
+        mock_response(
+            app.extensions["oauthlib.client"], "eosc_aai", oauth_token_response
+        )
+
+        # User authorized the requests and is redirect back
+        resp = c.get(
+            url_for(
+                "invenio_oauthclient.authorized",
+                remote_app="eosc_aai",
+                code="test",
+                state=get_state("eosc_aai"),
+            )
+        )
+        assert resp.status_code == 302
+
+        # Check that user is redirected to signup page
+        assert resp.location == url_for(
+            "invenio_oauthclient.signup", remote_app="eosc_aai"
+        )
+
+
+def test_authorized_signup_complete_flow(app_with_userprofiles, example_eosc_aai):
+    """Test complete authorized callback with sign-up flow."""
+    app = app_with_userprofiles
+    example_data, _ = example_eosc_aai
+    example_email = "jane.doe@example.org"
+
+    # Create OAuth token response format with user info
+    oauth_token_response = {
+        "access_token": "test_access_token",
+        "token_type": "Bearer",
+        "expires_in": 3600,
+        "refresh_token": "test_refresh_token",
+        "scope": "openid profile email",
+        # OpenID Connect includes user claims in token response
+        **example_data,
+    }
+
+    with app.test_client() as c:
+        # Ensure remote apps have been loaded
+        c.get(url_for("invenio_oauthclient.login", remote_app="eosc_aai"))
+
+        # Mock the response from EOSC AAI
+        mock_response(
+            app.extensions["oauthlib.client"], "eosc_aai", oauth_token_response
+        )
+
+        # User authorized the requests and is redirect back
+        resp = c.get(
+            url_for(
+                "invenio_oauthclient.authorized",
+                remote_app="eosc_aai",
+                code="test",
+                state=get_state("eosc_aai"),
+            )
+        )
+        assert resp.status_code == 302
+        assert resp.location == url_for(
+            "invenio_oauthclient.signup", remote_app="eosc_aai"
+        )
+
+        # User loads sign-up page
+        resp = c.get(url_for("invenio_oauthclient.signup", remote_app="eosc_aai"))
+        assert resp.status_code == 200
+
+        # Get account info from session
+        account_info = session[token_session_key("eosc_aai") + "_account_info"]
+
+        # User fills form to register
+        data = {
+            "email": example_email,
+            "password": "test123456",
+            "profile.username": "janedoe",
+            "profile.full_name": account_info["user"]["profile"]["full_name"],
+            "profile.affiliations": "EOSC",
+        }
+
+        # User submits signup form
+        resp = c.post(
+            url_for("invenio_oauthclient.signup", remote_app="eosc_aai"),
+            data=data,
+        )
+        assert resp.status_code == 302
+
+        # Assert database state (Sign-up complete)
+        user = User.query.filter_by(email=example_email).one()
+
+        # Check UserIdentity was created
+        UserIdentity.query.filter_by(
+            method="eosc_aai", id_user=user.id, id=example_data["sub"]
+        ).one()
+
+        # Check RemoteAccount was created
+        remote_account = RemoteAccount.query.filter_by(user_id=user.id).one()
+
+        # Check RemoteToken was created
+        RemoteToken.query.filter_by(id_remote_account=remote_account.id).one()
+
+        # Check user profile data
+        assert user.user_profile["full_name"] == "Jane Doe"
+        assert user.active
+        # EOSC AAI is configured with auto_confirm=False, so user needs confirmation
+        assert user.confirmed_at is None
+
+        # Check EOSC AAI specific data is stored
+        assert remote_account.extra_data["sub"] == example_data["sub"]
+        assert (
+            remote_account.extra_data["eunode_projects"]
+            == example_data["eunode_projects"]
+        )
+        assert remote_account.extra_data["entitlements"] == example_data["entitlements"]
+
+        # Test disconnect - should not work as it's the only login method
+        resp = c.get(url_for("invenio_oauthclient.disconnect", remote_app="eosc_aai"))
+        assert resp.status_code == 400
+
+        # Verify account still exists
+        user = User.query.filter_by(email=example_email).one()
+        assert (
+            1
+            == UserIdentity.query.filter_by(
+                method="eosc_aai", id_user=user.id, id=example_data["sub"]
+            ).count()
+        )
+
+        # Set a password for the user
+        user.password = hash_password("1234")
+        db.session.commit()
+
+        # Now disconnect should work
+        resp = c.get(url_for("invenio_oauthclient.disconnect", remote_app="eosc_aai"))
+        assert resp.status_code == 302
+
+        # User still exists but OAuth connection is removed
+        user = User.query.filter_by(email=example_email).one()
+        assert (
+            0
+            == UserIdentity.query.filter_by(
+                method="eosc_aai", id_user=user.id, id=example_data["sub"]
+            ).count()
+        )
+        assert RemoteAccount.query.filter_by(user_id=user.id).count() == 0
+        assert RemoteToken.query.count() == 0
+
+
+def test_authorized_reject(app):
+    """Test a rejected authorization request."""
+    with app.test_client() as c:
+        c.get(url_for("invenio_oauthclient.login", remote_app="eosc_aai"))
+
+        # User denies authorization
+        resp = c.get(
+            url_for(
+                "invenio_oauthclient.authorized",
+                remote_app="eosc_aai",
+                error="access_denied",
+                error_description="User denied access",
+                state=get_state("eosc_aai"),
+            )
+        )
+        assert resp.status_code in (301, 302)
+        assert resp.location == "/"
+
+        # Check message flash
+        assert session["_flashes"][0][0] == "info"
+
+
+def test_authorized_already_authenticated(app, models_fixture, example_eosc_aai):
+    """Test authorized callback when user is already authenticated."""
+    datastore = app.extensions["invenio-accounts"].datastore
+    login_manager = app.login_manager
+
+    example_data, _ = example_eosc_aai
+    existing_email = "existing@inveniosoftware.org"
+    user = datastore.find_user(email=existing_email)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return user
+
+    @app.route("/foo_login")
+    def login():
+        login_user(user)
+        return "Logged In"
+
+    # Create OAuth token response format with user info
+    oauth_token_response = {
+        "access_token": "test_access_token",
+        "token_type": "Bearer",
+        "expires_in": 3600,
+        "refresh_token": "test_refresh_token",
+        "scope": "openid profile email",
+        **example_data,
+    }
+
+    with app.test_client() as client:
+        # Make a fake login
+        client.get("/foo_login", follow_redirects=True)
+
+        # Ensure remote apps have been loaded
+        client.get(url_for("invenio_oauthclient.login", remote_app="eosc_aai"))
+
+        # Mock access token request
+        mock_response(
+            app.extensions["oauthlib.client"], "eosc_aai", oauth_token_response
+        )
+
+        # User goes to 'Linked accounts' and clicks 'Connect'
+        resp = client.get(
+            url_for(
+                "invenio_oauthclient.login", remote_app="eosc_aai", next="/someurl/"
+            )
+        )
+        assert resp.status_code == 302
+
+        # User authorized the requests and is redirected back
+        resp = client.get(
+            url_for(
+                "invenio_oauthclient.authorized",
+                remote_app="eosc_aai",
+                code="test",
+                state=get_state("eosc_aai"),
+            )
+        )
+        assert resp.status_code == 302
+
+        # Assert database state (Linking complete)
+        u = User.query.filter_by(email=existing_email).one()
+
+        # Check UserIdentity was created
+        UserIdentity.query.filter_by(
+            method="eosc_aai", id_user=u.id, id=example_data["sub"]
+        ).one()
+
+        # Check RemoteAccount was created
+        remote_account = RemoteAccount.query.filter_by(user_id=u.id).one()
+
+        # Check EOSC AAI data is stored
+        assert remote_account.extra_data["sub"] == example_data["sub"]
+        assert (
+            remote_account.extra_data["eunode_projects"]
+            == example_data["eunode_projects"]
+        )
+
+        # Disconnect link should work since user has other login methods
+        resp = client.get(
+            url_for("invenio_oauthclient.disconnect", remote_app="eosc_aai")
+        )
+        assert resp.status_code == 302
+
+        # User exists but EOSC AAI link is removed
+        u = User.query.filter_by(email=existing_email).one()
+        assert (
+            0
+            == UserIdentity.query.filter_by(
+                method="eosc_aai", id_user=u.id, id=example_data["sub"]
+            ).count()
+        )
+
+
+def test_not_authenticated(app):
+    """Test disconnect when user is not authenticated."""
+    with app.test_client() as client:
+        assert not current_user.is_authenticated
+        resp = client.get(
+            url_for("invenio_oauthclient.disconnect", remote_app="eosc_aai")
+        )
+        assert resp.status_code == 302
+
+
+def test_project_entitlements_extraction(example_eosc_aai):
+    """Test that EOSC AAI specific claims are properly extracted."""
+    example_data, _ = example_eosc_aai
+
+    # Test project extraction
+    projects = example_data.get("eunode_projects", [])
+    assert len(projects) == 3
+    assert "pp-0190356a-ac97-db53-21c0-df7cd31a47c4" in projects  # Personal project
+    assert "gp-01903568-c385-49ba-0356-1b4ac60a90ec" in projects  # Group project 1
+    assert "gp-0190356b-25a5-4f61-f926-38f9e0cd541a" in projects  # Group project 2
+
+    # Test entitlements extraction
+    entitlements = example_data.get("entitlements", [])
+    assert len(entitlements) == 3
+
+    # Check specific entitlement formats
+    assert any("role=owner" in ent for ent in entitlements)
+    assert any("role=member" in ent for ent in entitlements)
+    assert any("pp-" in ent for ent in entitlements)  # Personal project
+    assert any("gp-" in ent for ent in entitlements)  # Group project
