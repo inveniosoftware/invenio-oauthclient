563bf9d481ae423cd358d94057e33c6e783da4a5
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-01-27 22:27:40 +0100

    release: v6.1.2

--- invenio_oauthclient/__init__.py
+++ invenio_oauthclient/__init__.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2025 CERN.
-# Copyright (C) 2024 Graz University of Technology.
+# Copyright (C) 2024-2026 Graz University of Technology.
 # Copyright (C) 2025 KTH Royal Institute of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
@@ -14,7 +14,7 @@ from .ext import InvenioOAuthClient, InvenioOAuthClientREST
 from .oauth import oauth_link_external_id, oauth_unlink_external_id
 from .proxies import current_oauthclient
 
-__version__ = "6.1.1"
+__version__ = "6.1.2"
 
 __all__ = (
     "__version__",
