f026b971a4b88d4cdc9fe6b9662fad988bb1345a
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-01-27 22:27:26 +0100

    chore(black): update formatting to >= 26.0

--- invenio_oauthclient/handlers/token.py
+++ invenio_oauthclient/handlers/token.py
@@ -7,6 +7,7 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 
 """Funcs to manage tokens."""
+
 from datetime import datetime, timedelta, timezone
 from functools import partial
 
