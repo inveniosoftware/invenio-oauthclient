899d66b7a70e418bc57966f0e97fbef27489f75a
Mirek Simek <miroslav.simek@cesnet.cz>
2026-05-13 10:44:19 +0200

    fix: bad downstream dependency in alembic
    * The initial migration was generated with invenio-db as a downstream,
      which creates multiple "default" heads, disrupting alembic revision
    * The fix is to use plain depends_on instead of downstream revision
    * Tests for upgrade/downgrade added

--- invenio_oauthclient/alembic/44ab9963e8cf_create_oauthclient_branch.py
+++ invenio_oauthclient/alembic/44ab9963e8cf_create_oauthclient_branch.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "44ab9963e8cf"
-down_revision = "dbdbc1b19cf2"
+down_revision = None
 branch_labels = ("invenio_oauthclient",)
 depends_on = "dbdbc1b19cf2"
 
