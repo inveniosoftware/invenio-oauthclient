4fec53eef837ece30c316ce8260bac4b2a32888a
Pal Kerecsenyi <pal@p.kerecs.com>
2026-09-17 16:23:43 +0200

    release: v9.1.2

--- invenio_oauthclient/__init__.py
+++ invenio_oauthclient/__init__.py
@@ -10,7 +10,7 @@ from .ext import InvenioOAuthClient, InvenioOAuthClientREST
 from .oauth import oauth_link_external_id, oauth_unlink_external_id
 from .proxies import current_oauthclient
 
-__version__ = "9.1.1"
+__version__ = "9.1.2"
 
 __all__ = (
     "__version__",
